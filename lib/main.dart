2a07012957747e5cbbcc553097c02dcbb624b15e
ziehro <ziehro@gmail.com>
2025-12-16 14:35:06 -0700

    fffff

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:just_audio/just_audio.dart';
 import 'package:audio_service/audio_service.dart';
 import 'dart:io';
 import 'dart:math';
+import 'dart:async';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 import 'audio_track_selector.dart';
 
@@ -519,6 +521,8 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> with WidgetsBindi
   List<Map<String, dynamic>> _audioTracks = [];
   int _selectedTrackGroupIndex = 0;
   int _selectedTrackIndex = 0;
+  bool _showControls = true;
+  Timer? _hideTimer;
 
   @override
   void initState() {
@@ -530,8 +534,28 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> with WidgetsBindi
         if (mounted) {
           setState(() => _isInitialized = true);
           _controller.play();
+          WakelockPlus.enable();
+          _startHideTimer();
         }
       });
+  }
+
+  void _startHideTimer() {
+    _hideTimer?.cancel();
+    _hideTimer = Timer(const Duration(seconds: 2), () {
+      if (mounted) {
+        setState(() => _showControls = false);
+      }
+    });
+  }
+
+  void _toggleControls() {
+    setState(() {
+      _showControls = !_showControls;
+      if (_showControls) {
+        _startHideTimer();
+      }
+    });
   }
 
   Future<void> _loadAudioTracks() async {
@@ -601,11 +625,13 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> with WidgetsBindi
       if (_controller.value.isPlaying && audioHandler != null) {
         final position = _controller.value.position;
         _controller.pause();
+        WakelockPlus.disable();
         _startBackgroundAudio(position);
       }
     } else if (state == AppLifecycleState.resumed) {
       if (_isPlayingInBackground) {
         _resumeVideoFromAudio();
+        WakelockPlus.enable();
       }
     }
   }
@@ -637,6 +663,8 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> with WidgetsBindi
 
   @override
   void dispose() {
+    _hideTimer?.cancel();
+    WakelockPlus.disable();
     WidgetsBinding.instance.removeObserver(this);
     if (_isPlayingInBackground && audioHandler != null) {
       audioHandler!.stop();
@@ -662,46 +690,77 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> with WidgetsBindi
             ),
         ],
       ),
-      body: Center(
-        child: _isInitialized
-            ? Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: VideoPlayer(_controller),
-            ),
-            VideoProgressIndicator(_controller, allowScrubbing: true),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: _isInitialized
+          ? GestureDetector(
+        onTap: _toggleControls,
+        child: Container(
+          color: Colors.black,
+          child: Center(
+            child: Stack(
+              alignment: Alignment.center,
               children: [
-                IconButton(
-                  icon: Icon(_controller.value.isPlaying ? Icons.pause : Icons.play_arrow),
-                  onPressed: () {
-                    setState(() {
-                      _controller.value.isPlaying ? _controller.pause() : _controller.play();
-                    });
-                  },
+                AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
                 ),
-                IconButton(
-                  icon: Icon(_controller.value.volume > 0 ? Icons.volume_up : Icons.volume_off),
-                  onPressed: () {
-                    setState(() {
-                      _controller.setVolume(_controller.value.volume > 0 ? 0 : 1);
-                    });
-                  },
-                ),
+                if (_showControls)
+                  Container(
+                    color: Colors.black54,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        VideoProgressIndicator(_controller, allowScrubbing: true),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              icon: Icon(
+                                _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                                size: 48,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  if (_controller.value.isPlaying) {
+                                    _controller.pause();
+                                    _hideTimer?.cancel();
+                                  } else {
+                                    _controller.play();
+                                    _startHideTimer();
+                                  }
+                                });
+                              },
+                            ),
+                            IconButton(
+                              icon: Icon(
+                                _controller.value.volume > 0 ? Icons.volume_up : Icons.volume_off,
+                                size: 32,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _controller.setVolume(_controller.value.volume > 0 ? 0 : 1);
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                        if (_isPlayingInBackground)
+                          const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              'Audio playing in background',
+                              style: TextStyle(color: Colors.green),
+                            ),
+                          ),
+                        const SizedBox(height: 20),
+                      ],
+                    ),
+                  ),
               ],
             ),
-            if (_isPlayingInBackground)
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text('Audio playing in background', style: TextStyle(color: Colors.green)),
-              ),
-          ],
-        )
-            : const CircularProgressIndicator(),
-      ),
+          ),
+        ),
+      )
+          : const Center(child: CircularProgressIndicator()),
     );
   }
 }
